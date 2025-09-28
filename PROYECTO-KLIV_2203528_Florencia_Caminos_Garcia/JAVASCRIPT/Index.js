999cce9e64f9fd0e9ac9c2eba4866f4c028a256a
Gilnonima <florcaminosgarcia@gmail.com>
2025-09-27 23:07:19 -0300

    Update: Inicalización correcta

--- PROYECTO-KLIV_2203528_Florencia_Caminos_Garcia/JAVASCRIPT/Index.js
+++ PROYECTO-KLIV_2203528_Florencia_Caminos_Garcia/JAVASCRIPT/Index.js
@@ -1,11 +1,47 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     inicializeApp();
+
+});
+
+function inicializeApp() {
     setupHeaderEvents();
     setupSidebarEvents();
     setupDashboardEvents();
-    setupProjectsEvents(); 
-    setupTasksEvents(); 
-    setupSettingsEvents(); 
-    setupHelpEvents(); 
+    setupProjectsEvents();
+    setupTasksEvents();
+    setupSettingsEvents();
+    setupHelpEvents();
     setupGlobalEvents();
-});
+}
+
+function setupHeaderEvents() {
+    
+}
+
+function setupSidebarEvents() {
+
+}
+
+function setupDashboardEvents() {
+
+}
+
+function setupProjectsEvents() {
+
+}
+
+function setupTasksEvents() {
+
+}
+
+function setupSettingsEvents() {
+
+}
+
+function setupHelpEvents() {
+
+}
+
+function setupGlobalEvents() {
+
+}          
